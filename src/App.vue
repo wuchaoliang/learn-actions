d16d7ad23f72a1622cb81f1efa6786658e8131b9
wu.chaoliang <chaoliangwu9088@gmail.com>
2020-04-12 00:34:26 +0800

    测试SamKirkland/FTP-Deploy-Action@3.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home2</router-link> |
+      <router-link to="/">Home1</router-link> |
       <router-link to="/about">About1</router-link>
     </div>
     <router-view />
